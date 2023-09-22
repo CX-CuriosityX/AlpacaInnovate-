6ca944ac4f08c4c460e14aa47bd616058eddf1ee
CX-CuriosityX <94640281+CX-CuriosityX@users.noreply.github.com>
2023-09-22 14:14:52 +0530

    Add files via upload

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Security;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualBasic;
+
+namespace AlpacaInnovateDotnet.Pages;
+
+public class IndexModel : PageModel
+{
+    
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+        
+    }
+    
+    public void OnGet()
+    {
+   
+    }
+}
